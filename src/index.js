2a1bc65ac9505df3f569c769dd7e7b230d8b6fa6
m31271n <m31271n@users.noreply.github.com>
2018-11-14 15:24:43 +0800

    feat: add wrapper and test

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import DollarRecognizer, { Point } from './vendor/dollar'
+
+const pointMaker = ({ x, y }) => new Point(x, y)
+
+// wrap DollarRecognizer's API in JavaScript style
+class GestureRecognizer {
+  constructor() {
+    this.dollarRecognizer = new DollarRecognizer()
+  }
+
+  recognize(points, useProtractor) {
+    const result = this.dollarRecognizer.Recognize(
+      points.map(pointMaker),
+      useProtractor
+    )
+
+    const name = result.Name
+    return name
+  }
+
+  add(name, points) {
+    return this.dollarRecognizer.AddGesture(name, points.map(pointMaker))
+  }
+
+  reset() {
+    return this.DeleteUserGestures()
+  }
+}
+
+export default GestureRecognizer
--- test/index.js
+++ test/index.js
@@ -1,9 +1,17 @@
 import test from 'ava'
-import fn from '../src'
+import GestureRecognizer from '../src'
 
-test('title', t => {
-  const err = t.throws(() => fn(123), TypeError)
-  t.is(err.message, 'Expected a string, got number')
+const strokeZigZag = [
+  { x: 310, y: 230 },
+  { x: 333, y: 186 },
+  { x: 356, y: 215 },
+  { x: 375, y: 186 },
+  { x: 399, y: 216 },
+  { x: 418, y: 186 },
+]
 
-  t.is(fn('unicorns'), 'unicorns & rainbows')
+test('current wrapper worked', t => {
+  const gr = new GestureRecognizer()
+  t.is(gr.recognize(strokeZigZag, true), 'zig-zag')
+  t.is(gr.recognize(strokeZigZag, false), 'zig-zag')
 })
