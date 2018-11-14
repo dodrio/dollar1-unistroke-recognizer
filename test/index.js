4b0546f9aaae744d0fe31887c3dbe8dd2acfd1ac
m31271n <m31271n@users.noreply.github.com>
2018-11-14 23:02:36 +0800

    test: add useful tests

--- test/index.js
+++ test/index.js
@@ -1,17 +1,16 @@
 import test from 'ava'
 import GestureRecognizer from '../src'
+import { strokeZigZag, strokeS1, strokeS2 } from './_strokes'
 
-const strokeZigZag = [
-  { x: 310, y: 230 },
-  { x: 333, y: 186 },
-  { x: 356, y: 215 },
-  { x: 375, y: 186 },
-  { x: 399, y: 216 },
-  { x: 418, y: 186 },
-]
-
-test('current wrapper worked', t => {
+test('default strokes are working', t => {
   const gr = new GestureRecognizer()
   t.is(gr.recognize(strokeZigZag, true), 'zig-zag')
   t.is(gr.recognize(strokeZigZag, false), 'zig-zag')
+})
+
+test('add a custom stroke - S', t => {
+  const gr = new GestureRecognizer()
+  t.not(gr.recognize(strokeS2, false), 's')
+  gr.add('s', strokeS1)
+  t.is(gr.recognize(strokeS2, false), 's')
 })
